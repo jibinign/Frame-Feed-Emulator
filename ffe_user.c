870aa832487e23e48f3a3168d94cfaffad250411
jibinign <jibin.george@ignitarium.com>
2021-01-21 11:15:31 +0530

    User program for Frame Feed Emulator
    This program initialize frame feed emulator. The frame size is set by user.

--- ffe_user.c
+++ ffe_user.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -11,13 +13,14 @@
 #define V720P _IOW('a', 'd', int*)
 #define V1080P _IOW('a', 'e', int*)
 
-int ff_initialize(int fd, int cmd, int count);
+static int ff_initialize(int fd, int cmd, int count);
 
 int main(void)
 {
 	int fd;
 	int count = 1;		//By default frame count is 1
 	int cmd;		//Resolution
+
 	fd = open("/dev/ffemulator", O_RDWR);
 	if (fd < 0) {
 		printf("device file cannot be opened..\n");
@@ -28,32 +31,30 @@ int main(void)
 	scanf("%d", &cmd);
 	if (ff_initialize(fd, cmd, count))
 		return 0;
-	
 	close(fd);
 	return 0;
 }
 
-int ff_initialize(int fd, int cmd, int count)
+static int ff_initialize(int fd, int cmd, int count)
 {
 	switch (cmd) {
-		case 240:
-			ioctl(fd, V240P, (int *) &count);
-			break;
-		case 360:
-			ioctl(fd, V360P, (int *) &count);
-			break;
-		case 480:
-			ioctl(fd, V480P, (int *) &count);
-			break;
-		case 720:
-			ioctl(fd, V720P, (int *) &count);
-			break;
-		case 1080:
-			ioctl(fd, V1080P, (int *) &count);
-			break;
-		default:
-			printf("sorry..! The option entered is not valid..\n");
-			return -1;
-			break;
+	case 240:
+		ioctl(fd, V240P, (int *) &count);
+		break;
+	case 360:
+		ioctl(fd, V360P, (int *) &count);
+		break;
+	case 480:
+		ioctl(fd, V480P, (int *) &count);
+		break;
+	case 720:
+		ioctl(fd, V720P, (int *) &count);
+		break;
+	case 1080:
+		ioctl(fd, V1080P, (int *) &count);
+		break;
+	default:
+		printf("sorry..! The option entered is not valid..\n");
+		return -1;
 	}
 }
